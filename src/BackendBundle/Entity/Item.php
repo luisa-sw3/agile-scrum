a43f4d14056e0523ddbcf8a7db66b3a89b1175b5
Luisa Pereira <luisita231@gmail.com>
2016-05-11 11:00:13 -0500

    Generar PDF
    Se implementa la funcionalidad de descargar los reportes en PDF.
    
    Nota:
    * composer require h4cc/wkhtmltopdf-amd64
    * composer update
    * en app/config/config.yml cambiar el binary de windows a
    %kernel.root_dir%/../vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64

--- src/BackendBundle/Entity/Item.php
+++ src/BackendBundle/Entity/Item.php
@@ -39,7 +39,7 @@ class Item {
     const STATUS_CONFIRMED = 3;
     const STATUS_NOT_A_BUG = 4;
     const STATUS_BEING_WORKED_ON = 5;
-    const STATUS_NEAR_COMPLETION = 6;
+    const STATUS_BUG_DETECTED = 6;
     const STATUS_READY_FOR_TESTING = 7;
     const STATUS_TESTING = 8;
     const STATUS_CANCELED = 9;
@@ -109,6 +109,12 @@ class Item {
      * @ORM\Column(name="item_priority", type="float", nullable=true)
      */
     protected $priority;
+
+    /**
+     * Variable numérica para saber en que ciclo se ha corregido un error
+     * @ORM\Column(name="item_fixed_on_cycle", type="integer", nullable=true)
+     */
+    protected $fixedOnCycle;
 
     /**
      * Usuario quien realiza la creación del item
@@ -221,6 +227,10 @@ class Item {
         return $this->parent;
     }
 
+    function getFixedOnCycle() {
+        return $this->fixedOnCycle;
+    }
+
     function setTitle($title) {
         $this->title = $title;
     }
@@ -287,6 +297,10 @@ class Item {
 
     function setEffortTShirt($effortTShirt) {
         $this->effortTShirt = $effortTShirt;
+    }
+
+    function setFixedOnCycle($fixedOnCycle) {
+        $this->fixedOnCycle = $fixedOnCycle;
     }
 
     /**
@@ -417,8 +431,8 @@ class Item {
             case self::STATUS_BEING_WORKED_ON:
                 $langVar = 'backend.item.status_being_worked_on';
                 break;
-            case self::STATUS_NEAR_COMPLETION:
-                $langVar = 'backend.item.status_near_completion';
+            case self::STATUS_BUG_DETECTED:
+                $langVar = 'backend.item.status_bug_detected';
                 break;
             case self::STATUS_READY_FOR_TESTING:
                 $langVar = 'backend.item.status_ready_for_testing';
@@ -474,8 +488,8 @@ class Item {
                 $class = 'label label-default label-notification';
                 $border = 'border-status-3';
                 break;
-            case self::STATUS_NEAR_COMPLETION:
-                $class = 'label label-default label-notification';
+            case self::STATUS_BUG_DETECTED:
+                $class = 'label label-default label-danger';
                 $border = 'border-status-3';
                 break;
             case self::STATUS_READY_FOR_TESTING:
@@ -507,8 +521,8 @@ class Item {
         }
         if ($labelClass) {
             return $class;
-        } 
-        
+        }
+
         if ($borderClass) {
             return $border;
         }
@@ -588,7 +602,5 @@ class Item {
         }
         return $iconName;
     }
-    
-    
 
 }
--- src/BackendBundle/Entity/TimeTrackingRepository.php
+++ src/BackendBundle/Entity/TimeTrackingRepository.php
@@ -40,5 +40,47 @@ class TimeTrackingRepository extends EntityRepository {
 
         return $consult->getResult();
     }
+    
+    /**
+     * Permite obtener el total de segundos trabajado por un usuario
+     * acorde a los parametros de busqueda
+     * @param integer $userId identificador del usuario
+     * @param array[string] $search parameros de busqueda
+     * @return type
+     */
+    public function findWorkedTime($userId, $search = array()) {
+
+        $em = $this->getEntityManager();
+
+        $extraQuery = '';
+        $parameters = array('userId' => $userId);
+        if (isset($search['startDate'])) {
+            $extraQuery .= ' AND time.date >= :startDate ';
+            $parameters['startDate'] = $search['startDate'];
+        }
+        
+        if (isset($search['endDate'])) {
+            $extraQuery .= ' AND time.date <= :endDate ';
+            $parameters['endDate'] = $search['endDate'];
+        }
+        
+        if (isset($search['project'])) {
+            $extraQuery .= ' AND time.project = :project ';
+            $parameters['project'] = $search['project'];
+        }
+
+        $orderBy = " ORDER BY time.date DESC, time.startTime DESC ";
+        
+        $query = "
+        SELECT SUM(time.workedTime)
+        FROM BackendBundle:TimeTracking time
+        WHERE time.user = :userId 
+        AND time.endTime IS NOT NULL".$extraQuery.$orderBy;
+
+        $consult = $em->createQuery($query);
+        $consult->setParameters($parameters);
+
+        return $consult->getSingleScalarResult();
+    }
 
 }
